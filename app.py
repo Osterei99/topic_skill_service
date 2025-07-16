3cba09f0be180779cf16b8fbdaccf7baa1626f3b
Kai Brokmann <133008349+Osterei99@users.noreply.github.com>
2025-07-16 11:09:58 +0200

    try and except

--- app.py
+++ app.py
@@ -12,9 +12,17 @@ def hello():
 @app.route('/topics')
 def get_topics():
     data_path = os.path.join(os.path.dirname(__file__), 'data', 'topics.json')
-    with open(data_path, 'r') as f:
-        topics = json.load(f)
-    return jsonify(topics)
+    try:
+        with open(data_path, 'r') as f:
+            topics = json.load(f)
+        return jsonify(topics)
+    except json.JSONDecodeError
+        print(f"Fehler beim Dekodieren der JSON-Datei: {filepath}. Bitte JSON-Syntax überprüfen!")
+        return []
+    except Exception as e:
+        print(f"Ein unbekannter Fehler ist aufgetreten: {e}")
+        return []
+
 
 if __name__ == '__main__':
     app.run(debug=True,port=5000)
